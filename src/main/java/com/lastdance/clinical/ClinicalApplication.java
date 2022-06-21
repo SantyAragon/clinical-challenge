601de00fcaa9ecd02b723e3217af0d37e154704c
Santiago <santiago.aragon.99@gmail.com>
2022-06-21 03:12:06 -0300

    Initial commit

---
+++ src/main/java/com/lastdance/clinical/ClinicalApplication.java
@@ -0,0 +1,14 @@
+package com.lastdance.clinical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/lastdance/clinical/ClinicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastdance.clinical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
