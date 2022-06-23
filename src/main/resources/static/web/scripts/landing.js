84fc51de614ac73386a8cf8f0ce8ca725f574f09
iwdsoon <luchoziwd@gmail.com>
2022-06-23 11:45:51 -0300

    agregado comienzo de index

--- src/main/resources/static/web/scripts/landing.js
+++ src/main/resources/static/web/scripts/landing.js
@@ -5,16 +5,40 @@ Vue.createApp({
 
       }
     },
+
+    mounted(){
+
+      //Menu toggle-button for small screens
+      $(document).ready(function (){
+      $(".menu-icon").on("click", function() {
+      $("nav ul").toggleClass("showing");
+      });
+      });
+
+      //Scrolling Effect for nav
+      $(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+      $('nav').addClass('black');
+      } else {
+      $('nav').removeClass('black');
+      }
+      });
+
+    },
+    
     created() {
   
-      axios.get('/api/clients/current') // axios.get('http://localhost:8080/api/clients/current',{headers:{'accept':'application/xml'}})
-
+      axios.get('/api/clients/current')
+      .then (data =>{
+        this.client = data.data;
+      })
+      .catch(error => console.warn(error.message));
     },
   
     methods: {
 
-
     },
+
     computed: {
   
     }
