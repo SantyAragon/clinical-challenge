9477123402e4ea09bc04d9fc22fead7be233ce88
Santiago <santiago.aragon.99@gmail.com>
2022-06-24 11:42:50 -0300

    creamos factura controller y service, servlet para traer una/todas

--- src/main/java/com/lastdance/clinical/controllers/FacturaController.java
+++ src/main/java/com/lastdance/clinical/controllers/FacturaController.java
@@ -1,5 +1,31 @@
 package com.lastdance.clinical.controllers;
 
-public class FacturaController {
+import com.lastdance.clinical.DTOS.FacturaDTO;
+import com.lastdance.clinical.services.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api")
+public class FacturaController {
+    @Autowired
+    FacturaService facturaService;
+
+    @GetMapping("/facturas")
+    public Set<FacturaDTO> traerFacturas() {
+        return facturaService.traerFacturasDTO();
+    }
+
+    @GetMapping("/facturas/{id}")
+    public FacturaDTO traerFactura(@PathVariable Long id) {
+        return facturaService.traerFacturaDTO(id);
+    }
+
+    
 }
--- src/main/java/com/lastdance/clinical/repositories/FacturaRepository.java
+++ src/main/java/com/lastdance/clinical/repositories/FacturaRepository.java
@@ -1,9 +1,11 @@
 package com.lastdance.clinical.repositories;
 
 import com.lastdance.clinical.models.Factura;
+import com.lastdance.clinical.models.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
+    Factura findByPaciente(Paciente paciente);
 }
