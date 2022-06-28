5d38ade8351514b16bb4798fa396961703a87219
Santiago <santiago.aragon.99@gmail.com>
2022-06-28 02:14:49 -0300

    cambios diseño pdf

--- src/main/java/com/lastdance/clinical/services/implement/PdfServiceImplements.java
+++ src/main/java/com/lastdance/clinical/services/implement/PdfServiceImplements.java
@@ -3,6 +3,7 @@ package com.lastdance.clinical.services.implement;
 import com.lastdance.clinical.models.Factura;
 import com.lastdance.clinical.models.PacienteProducto;
 import com.lastdance.clinical.models.PacienteServicio;
+import com.lastdance.clinical.models.Profesional;
 import com.lastdance.clinical.services.FacturaService;
 import com.lastdance.clinical.services.PdfService;
 import com.lowagie.text.*;
@@ -18,6 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 
 @Service
 public class PdfServiceImplements implements PdfService {
@@ -28,6 +32,7 @@ public class PdfServiceImplements implements PdfService {
     @Override
     public void export(HttpServletResponse response, Long id) throws IOException, DocumentException {
         Factura factura = facturaService.traerFactura(id);
+
         Set<PacienteServicio> servicios = factura.getServicios();
         Set<PacienteProducto> productos = factura.getProductos();
 
@@ -35,8 +40,16 @@ public class PdfServiceImplements implements PdfService {
         PdfWriter.getInstance(document, response.getOutputStream());
 
         document.open();
+        Font fontFactura = FontFactory.getFont(FontFactory.COURIER_BOLD);
+        fontFactura.setSize(20);
+
+        Paragraph textoFactura = new Paragraph("FACTURA",fontFactura);
+        textoFactura.setAlignment(Element.ALIGN_TOP);
+        textoFactura.setAlignment(Element.ALIGN_RIGHT);
+        textoFactura.setSpacingAfter(5);
+
         Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA);
-        fontTitle.setSize(18);
+        fontTitle.setSize(14);
 
         Paragraph title = new Paragraph("Factura n°" + factura.getId(), fontTitle);
         title.setAlignment(Paragraph.ALIGN_MIDDLE);
@@ -46,21 +59,70 @@ public class PdfServiceImplements implements PdfService {
         paragraphServicios.setAlignment(Paragraph.ALIGN_LEFT);
         paragraphServicios.setSpacingAfter(5);
 
-        PdfPTable tableServicios = new PdfPTable(6);
+        PdfPTable tableServicios = new PdfPTable(5);
         tableServicios.setWidthPercentage(100);
 
-        java.util.List<String> headersServicios = java.util.List.of("ID", "Servicio", "Description", "Amount", "Date", "Profesional");
+        java.util.List<String> headersServicios = java.util.List.of("ID", "Servicio", "Monto", "Fecha", "Profesional");
 
         Font fontTabla = FontFactory.getFont(FontFactory.HELVETICA);
-        fontTitle.setSize(18);
+        fontTabla.setSize(12);
+        fontTabla.setColor(new Color(20, 189, 165));
+
 
         headersServicios.forEach(element -> {
-            PdfPCell c1 = new PdfPCell(new Phrase(element,fontTabla));
-            c1.setBackgroundColor(new Color(48,227,202));
+            PdfPCell c1 = new PdfPCell(new Phrase(element, fontTabla));
+//            c1.setBackgroundColor(new Color(48, 227, 202));
             c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c1.setBorderWidthLeft(0);
+            c1.setBorderWidthRight(0);
             tableServicios.addCell(c1);
         });
 
+        Comparator<PacienteServicio> idComparatorServ = Comparator.comparing(PacienteServicio::getId);
+
+        servicios.stream().sorted(idComparatorServ).forEach(servicio -> {
+            PdfPCell c1 = new PdfPCell(new Phrase(servicio.getId() + ""));
+            c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c1.setBorderWidth(0);
+            tableServicios.addCell(c1);
+
+            PdfPCell c2 = new PdfPCell(new Phrase(servicio.getServicio().getTipoServicio() + ""));
+            c2.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c2.setBorderWidth(0);
+            tableServicios.addCell(c2);
+
+            PdfPCell c3 = new PdfPCell(new Phrase(servicio.getServicio().getMonto() + ""));
+            c3.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c3.setBorderWidth(0);
+            tableServicios.addCell(c3);
+
+            PdfPCell c4 = new PdfPCell(new Phrase(servicio.getFecha().format(ISO_LOCAL_DATE) + ""));
+            c4.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c4.setBorderWidth(0);
+            tableServicios.addCell(c4);
+
+            PdfPCell c5 = new PdfPCell(new Phrase(servicio.getServicio().getProfesionales().stream().map(Profesional::getFullName).collect(Collectors.toSet()) + ""));
+            c5.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c5.setBorderWidth(0);
+            tableServicios.addCell(c5);
+
+        });
+
+        PdfPCell subtotalServicios = new PdfPCell(new Phrase("Subtotal servicios:", fontTabla));
+        subtotalServicios.setColspan(4);
+        subtotalServicios.setPaddingTop(4);
+        subtotalServicios.setBorderWidth(0);
+//        subtotalProductos.setBackgroundColor(new Color(48, 227, 202));
+        subtotalServicios.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        tableServicios.addCell(subtotalServicios);
+
+        Double totalServicios = servicios.stream().map(PacienteServicio::getMonto).reduce(Double::sum).orElse(0d);
+        PdfPCell precioServicios = new PdfPCell(new Phrase(totalServicios + "", fontTabla));
+        precioServicios.setPaddingTop(4);
+        precioServicios.setBorderWidth(0);
+//        precioProductos.setBackgroundColor(new Color(217, 217, 217));
+        precioServicios.setHorizontalAlignment(Element.ALIGN_CENTER);
+        tableServicios.addCell(precioServicios);
 
 
         Paragraph paragraphProductos = new Paragraph("Productos comprados");
@@ -71,20 +133,88 @@ public class PdfServiceImplements implements PdfService {
         PdfPTable tableProductos = new PdfPTable(5);
         tableProductos.setWidthPercentage(100);
 
-        java.util.List<String> headersProductos = java.util.List.of("IdProducto", "Producto", "Cantidad", "Monto", "Fecha");
+        java.util.List<String> headersProductos = java.util.List.of("Id Producto", "Producto", "Cantidad", "Precio unitario", "Monto");
+
 
         headersProductos.forEach(element -> {
-            PdfPCell c1 = new PdfPCell(new Phrase(element,fontTabla));
-            c1.setBackgroundColor(new Color(48,227,202));
+            PdfPCell c1 = new PdfPCell(new Phrase(element, fontTabla));
+//            c1.setBackgroundColor(new Color(48, 227, 202));
             c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c1.setBorderWidthLeft(0);
+            c1.setBorderWidthRight(0);
             tableProductos.addCell(c1);
         });
 
+        Integer number = 1;
+
+        Comparator<PacienteProducto> idComparatorProd = Comparator.comparing(PacienteProducto::getId);
+
+        productos.stream().sorted(idComparatorProd).forEach(producto -> {
+
+            PdfPCell c1 = new PdfPCell(new Phrase(producto.getProducto().getId() + ""));
+            c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c1.setBorderWidth(0);
+            tableProductos.addCell(c1);
+
+            PdfPCell c2 = new PdfPCell(new Phrase(producto.getProducto().getNombre() + ""));
+            c2.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c2.setBorderWidth(0);
+            tableProductos.addCell(c2);
+
+            PdfPCell c3 = new PdfPCell(new Phrase(producto.getCantidad() + ""));
+            c3.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c3.setBorderWidth(0);
+            tableProductos.addCell(c3);
+
+            PdfPCell c4 = new PdfPCell(new Phrase(producto.getProducto().getPrecio() + ""));
+            c4.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c4.setBorderWidth(0);
+            tableProductos.addCell(c4);
+
+            PdfPCell c5 = new PdfPCell(new Phrase(producto.getMonto() + ""));
+            c5.setHorizontalAlignment(Element.ALIGN_CENTER);
+            c5.setBorderWidth(0);
+            tableProductos.addCell(c5);
+
+
+        });
+
+        PdfPCell subtotalProductos = new PdfPCell(new Phrase("Subtotal productos:", fontTabla));
+        subtotalProductos.setColspan(4);
+        subtotalProductos.setPaddingTop(4);
+        subtotalProductos.setBorderWidth(0);
+//        subtotalProductos.setBackgroundColor(new Color(48, 227, 202));
+        subtotalProductos.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        tableProductos.addCell(subtotalProductos);
+
+        Double totalProductos = productos.stream().map(PacienteProducto::getMonto).reduce(Double::sum).orElse(0d);
+        PdfPCell precioProductos = new PdfPCell(new Phrase(totalProductos + "", fontTabla));
+        precioProductos.setPaddingTop(4);
+        precioProductos.setBorderWidth(0);
+//        precioProductos.setBackgroundColor(new Color(217, 217, 217));
+        precioProductos.setHorizontalAlignment(Element.ALIGN_CENTER);
+        tableProductos.addCell(precioProductos);
+
+        document.add(textoFactura);
         document.add(title);
         document.add(paragraphServicios);
         document.add(tableServicios);
         document.add(paragraphProductos);
         document.add(tableProductos);
         document.close();
+    }
+
+    public static boolean esPar(Integer number) {
+        return number % 2 == 0;
+    }
+
+    public static void changeColor(Integer number, PdfPCell newCell, Font font) {
+        if (esPar(number)) {
+            newCell.setBackgroundColor(Color.getHSBColor(0.23251028F, 0.3266129F, 0.972549F));
+            font.setColor(Color.DARK_GRAY);
+        } else {
+            newCell.setBackgroundColor(Color.WHITE);
+            font.setColor(Color.BLACK);
+        }
     }
 }
