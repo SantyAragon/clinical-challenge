4f193ce44ae753b6360d1618d04c4738d62f083b
iwdsoon <luchoziwd@gmail.com>
2022-06-29 12:51:21 -0300

    commit para mergear

--- src/main/resources/static/web/scripts/index.js
+++ src/main/resources/static/web/scripts/index.js
@@ -52,30 +52,6 @@ Vue.createApp({
         })
       });
 
-
-// Smooth Scrolling Using Navigation Menu
-// $(function() {
-//     $('a.page-scroll').bind('click', function(event) {
-//         var $anchor = $(this);
-//         $('html, body').stop().animate({
-//             scrollTop: $($anchor.attr('href')).offset().top
-//         }, 1500, 'easeInOutExpo');
-//         event.preventDefault();
-//     });
-// });
-
-// // Highlight the top nav as scrolling occurs
-// $('body').scrollspy({
-//     target: '.navbar-fixed-top'
-// })
-
-// // Closes the Responsive Menu on Menu Item Click
-// $('.navbar-collapse ul li a').click(function() {
-//     $('.navbar-toggle:visible').click();
-// });
-
-    
-
       // Loader
       $(document).ready(function preloaderSetup() {
       $(".st-perloader").fadeOut();
